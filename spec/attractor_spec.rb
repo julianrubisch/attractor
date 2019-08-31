b2b96ceabf3b530ad3a1098b02435b8830dd6415
Julian Rubisch <julian@julianrubisch.at>
2019-08-31 22:04:36 +0200

    Spec the intended behavior #8

--- spec/attractor_spec.rb
+++ spec/attractor_spec.rb
@@ -1,5 +1,18 @@
+# frozen_string_literal: true
+
 RSpec.describe Attractor do
-  it "has a version number" do
+  it 'has a version number' do
     expect(Attractor::VERSION).not_to be nil
+  end
+
+  it 'suggests the 95 percentile refactoring candidated' do
+    values = 1.upto(30).map do |index|
+      Attractor::Value.new(file_path: "file_#{index}.rb", churn: index, complexity: index)
+    end
+
+    suggestions = Attractor::Calculator.get_suggestions(values)
+
+    expect(suggestions.map(&:file_path)).to contain_exactly 'file_30.rb', 'file_29.rb'
+    expect(suggestions.map(&:file_path)).not_to include 'file_28.rb'
   end
 end
