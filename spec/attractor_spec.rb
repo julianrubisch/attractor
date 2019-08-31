00bd77961901ae88123f1b8b99b1c39b66ea9c1c
Julian Rubisch <julian@julianrubisch.at>
2019-08-31 20:52:20 +0200

    Delete unused spec #8

--- spec/attractor_spec.rb
+++ spec/attractor_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe Attractor do
   it "has a version number" do
     expect(Attractor::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
