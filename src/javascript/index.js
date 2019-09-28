9af4a9761cb9a7c1f20eb159d0b3b021ca08c8e2
Julian Rubisch <julian@julianrubisch.at>
2019-09-28 20:21:08 +0200

    Add React and render Suggestions component #28

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,3 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
 import { chart } from "./functions";
+import Suggestions from "./components/Suggestions.jsx";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const wrapper = document.getElementById("suggestions");
+  if (wrapper) {
+    ReactDOM.render(<Suggestions />, wrapper);
+  }
+});
 
 window.chart = chart;
