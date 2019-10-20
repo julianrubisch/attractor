c58742f1f0a04f80c8b4bdf2bcfbfdef635e2174
Julian Rubisch <julian@julianrubisch.at>
2019-10-20 16:27:52 +0200

    Add base calculator spec #47

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,2 +1,18 @@
-RSpec.describe Attractor::Calculator do
+RSpec.describe Attractor::BaseCalculator do
+  let(:churn_calc_instance) { instance_double(::Churn::ChurnCalculator) }
+
+  before do
+    allow(::Churn::ChurnCalculator).to receive(:new).and_return(churn_calc_instance)
+  end
+  
+  it 'yields control with the change from churn' do
+    changes = [{ times_changed: 5, file_path: 'lib/test.rb' }]
+    allow(churn_calc_instance).to receive(:report).and_return(churn: { changes: changes })
+    calculator = described_class.new
+    calculator.calculate do |change|
+      [7, { some_method: 5, other_method: 2 }]
+    end
+
+    expect { |b| calculator.calculate(&b) }.to yield_with_args(changes.first)
+  end
 end
