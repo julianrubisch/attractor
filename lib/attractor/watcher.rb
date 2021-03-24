1e68908a00a984d05a354229d8d68d47cb12cc0a
Julian Rubisch <julian@julianrubisch.at>
2021-03-24 15:39:30 +0100

    Initialize empty file_prefix and require listen

--- lib/attractor/watcher.rb
+++ lib/attractor/watcher.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require "listen"
+
 module Attractor
   # functionality for watching file system changes
   class Watcher
@@ -11,7 +13,7 @@ module Attractor
     def watch
       @callback.call
 
-      listener = Listen.to(@file_prefix, ignore: /^attractor_output/) do |modified, _added, _removed|
+      listener = Listen.to(File.absolute_path(@file_prefix), ignore: /^attractor_output/) do |modified, _added, _removed|
         if modified
           puts "#{modified.map(&:to_s).join(', ')} modified, recalculating..."
           @callback.call
