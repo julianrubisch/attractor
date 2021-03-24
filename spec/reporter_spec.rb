a811ffe686197a073ae2bc1756362d50676a1944
Julian Rubisch <julian@julianrubisch.at>
2021-03-24 14:13:25 +0100

    Fix reporter values reader spec

--- spec/reporter_spec.rb
+++ spec/reporter_spec.rb
@@ -5,13 +5,13 @@ RSpec.describe Attractor::BaseReporter do
   before do
     allow(calc_dbl).to receive(:calculate).and_return(values)
   end
- 
+
   it 'renders something' do
-    expect(described_class.new(calculators: { 'rb': calc_dbl }).render).to eq 'Attractor'
+    expect(described_class.new(calculators: { 'rb' => calc_dbl }).render).to eq 'Attractor'
   end
 
   it 'allows injection of a calculator and calculates values' do
-    reporter = described_class.new(calculators: { 'rb': calc_dbl })
+    reporter = described_class.new(calculators: { 'rb' => calc_dbl })
     expect(reporter.values).to eq(values)
   end
 end
