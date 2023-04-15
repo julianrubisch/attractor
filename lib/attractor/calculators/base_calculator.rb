489948a18e9cd3bb889897f35a687b2fad408499
Julian Rubisch <julian@julianrubisch.at>
2023-04-15 18:46:52 +0200

    feat: Add verbose flag

--- lib/attractor/calculators/base_calculator.rb
+++ lib/attractor/calculators/base_calculator.rb
@@ -9,12 +9,13 @@ module Attractor
   class BaseCalculator
     attr_reader :type
 
-    def initialize(file_prefix: "", ignores: "", file_extension: "rb", minimum_churn_count: 3, start_ago: "5y")
+    def initialize(file_prefix: "", ignores: "", file_extension: "rb", minimum_churn_count: 3, start_ago: "5y", verbose: false)
       @file_prefix = file_prefix
       @file_extension = file_extension
       @minimum_churn_count = minimum_churn_count
       @start_date = Date.today - Attractor::DurationParser.new(start_ago).duration
       @ignores = ignores
+      @verbose = verbose
     end
 
     def calculate
@@ -26,7 +27,7 @@ module Attractor
         ignores: @ignores
       ).report(false)
 
-      puts "Calculating churn and complexity values for #{churn[:churn][:changes].size} #{type} files"
+      puts "Calculating churn and complexity values for #{churn[:churn][:changes].size} #{type} files" if @verbose
 
       values = churn[:churn][:changes].map do |change|
         history = git_history_for_file(file_path: change[:file_path])
@@ -47,13 +48,13 @@ module Attractor
           Cache.write(file_path: change[:file_path], value: value)
         end
 
-        print "."
+        print "." if @verbose
         value
       end
 
       Cache.persist!
 
-      print "\n\n"
+      print "\n\n" if @verbose
 
       values
     end
